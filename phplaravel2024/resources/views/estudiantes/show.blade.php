fac4d9d16a86b4a770fc8a9e9c50a7706136be2f
Odir123 <167135891+Odir123@users.noreply.github.com>
2024-07-01 15:13:48 -0600

    Update show.blade.php

--- phplaravel2024/resources/views/estudiantes/show.blade.php
+++ phplaravel2024/resources/views/estudiantes/show.blade.php
@@ -1,27 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>ver estudiante</h1>
+    <h1>Ver estudiante</h1>
     <div class="row">
         <div class="col-md-4">
             <label for="nombre" class="form-label">Nombre</label>
-            <input type="text" class="form-control" id="nombre" value="{{ $docente->nombre }}" disabled>
+            <input type="text" class="form-control" id="nombre" value="{{ $estudiante->nombre }}" disabled>
         </div>
         <div class="col-md-4">
             <label for="apellido" class="form-label">Apellido</label>
-            <input type="text" class="form-control" id="apellido" value="{{ $docente->apellido }}" disabled>
+            <input type="text" class="form-control" id="apellido" value="{{ $estudiante->apellido }}" disabled>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6">
             <label for="email" class="form-label">Correo electrónico</label>
-            <input type="email" class="form-control" id="email" name="email" value="{{ $docente->email }}" disabled>
+            <input type="email" class="form-control" id="email" name="email" value="{{ $estudiante->email }}" disabled>
         </div>
     </div>
     <br>
     <div class="row">
         <div class="col-md-12">
-            <a href="{{ route('docentes.index') }}" class="btn btn-primary">Retornar</a>
+            <a href="{{ route('estudiantes.index') }}" class="btn btn-primary">Retornar</a>
         </div>
     </div>
 @endsection
+
